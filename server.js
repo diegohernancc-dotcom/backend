78655c304d5359f0e4d30997f3825eee6726e56b
Diego Comina <diego.hernancc@gmail.com>
2026-06-19 11:55:37 -0500

    Conexion Render PostgreSQL

---
+++ server.js
@@ -0,0 +1,59 @@
+const express=require("express");
+const cors=require("cors");
+const {Pool}=require("pg");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const db=new Pool({
+connectionString:
+process.env.DATABASE_URL
+});
+
+app.get("/",(req,res)=>{
+
+res.send("API funcionando");
+
+});
+
+app.get(
+"/usuarios",
+async(req,res)=>{
+
+try{
+
+const resultado=
+await db.query(
+"SELECT * FROM usuarios"
+);
+
+res.json(
+resultado.rows
+);
+
+}
+catch(error){
+
+res.status(500)
+.send(error.message);
+
+}
+
+});
+
+const PORT=
+process.env.PORT||3000;
+
+app.listen(
+PORT,
+()=>{
+
+console.log(
+"Servidor activo"
+);
+
+});
